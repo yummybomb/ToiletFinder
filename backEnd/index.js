26ed935cc7d9d2b8294bc51851adee341a1e7646
Justin Cai <justin.cai@stonybrook.edu>
2022-09-24 21:16:52 -0400

    update backend

--- backEnd/index.js
+++ backEnd/index.js
@@ -2,12 +2,16 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 const port = 3000
+var axios = require('axios');
+app.use(express.json());
 
 const Client = require("@googlemaps/google-maps-services-js");
-//console.log(Client)
-/*
 const client = new Client.Client({});
-console.log(process.env.apikey)
+
+privateLocations = []; // Should hold object of {owner, name, latitude, longitude, vicinity}
+
+//console.log(client);
+/*console.log(process.env.apikey)
 client
   .elevation({
     params: {
@@ -25,40 +29,60 @@ client
 */
 
 // ROUTES------------------------- 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
 
+// Params: latitude, longitude
+// Return locations of all public bathrooms near this latitude and longitude
 app.get('/publiclocations', (req,res) => {
-    // Return locations of all public bathrooms near this latitude and longitude
-    // Params: latitude, longitude
-    client
-        .elevation({
-            params: {
-            locations: [{ lat: 45, lng: -110 }],
-            key: process.env.API_KEY,
-            },
-            timeout: 1000, // milliseconds
-        })
-        .then((r) => {
-            console.log(r.data.results[0].elevation);
-        })
-        .catch((e) => {
-            console.log(e.response.data.error_message);
-        });
-    res.send(req.url);
-})
-
-app.get('/privatelocations', (req,res) => {
-    // Return locations of all private submitted bathrooms
+    console.log(`getting public locations`);
+    const { latitude, longitude } = req.query;
+    //console.log(latitude);
+    var config = {
+        method: 'get',
+        url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude}%2C${longitude}&radius=800&keyword=public%20bathrooms&type=&key=${process.env.API_KEY}`
+    };
     
-    res.send(req.url);
+    axios(config)
+        .then(function (response) {
+            console.log(JSON.stringify(response.data.results));
+            response.data.results.forEach(element => {
+                console.log(`Name: ${element.name}, Location: Lat:${element.geometry.location.lat} Long:${element.geometry.location.lng}`);
+            })
+            res.json(response.data.results);
+        })
+        .catch(function (error) {
+            console.log(error);
+            res.json();
+        });
 })
 
+// Return locations of all private submitted bathrooms
+app.get('/privatelocations', (req,res) => { 
+    console.log("getting private locations")
+    res.status(200).json(privateLocations);
+})
+
+// Return locations of specific user's submitted bathrooms
 app.get('/locations/:user', (req,res) => {
-    // Return locations of specific user's submitted bathrooms
-    console.log(req.params.user);
-    res.send(req.url);
+    console.log(`getting ${req.params.user}'s locations`);
+    user = req.params.user;
+    returnArr = [];
+    privateLocations.forEach(element => {
+        if (element.owner == user)
+            returnArr.push(element);
+    })
+    res.json(returnArr);
+})
+
+// Adds location to list
+app.post('/locations', (req,res) => {
+    const {owner, name, latitude, longitude, vicinity} = req.body;
+    if (owner && name && latitude && longitude && vicinity) {
+        privateLocations.push({owner, name, latitude, longitude, vicinity});
+        res.status(200).json("success");
+    } else {
+        res.status(400).json("failed");
+    }
+
 })
 //-----------------------------------
 
