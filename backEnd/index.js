589f4ce8e047813870593e5a76b594b9357f3a5b
Justin Cai <justin.cai@stonybrook.edu>
2022-09-24 21:17:29 -0400

    remove commented code

--- backEnd/index.js
+++ backEnd/index.js
@@ -10,24 +10,6 @@ const client = new Client.Client({});
 
 privateLocations = []; // Should hold object of {owner, name, latitude, longitude, vicinity}
 
-//console.log(client);
-/*console.log(process.env.apikey)
-client
-  .elevation({
-    params: {
-      locations: [{ lat: 45, lng: -110 }],
-      key: process.env.apikey,
-    },
-    timeout: 1000, // milliseconds
-  })
-  .then((r) => {
-    console.log(r.data.results[0].elevation);
-  })
-  .catch((e) => {
-    console.log(e.response.data.error_message);
-  });
-*/
-
 // ROUTES------------------------- 
 
 // Params: latitude, longitude
